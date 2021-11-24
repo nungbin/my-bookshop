0287b80754c4ed9a07202599f5445f1700c3ca50
shubinlin <shubinlin@gmail.com>
2021-11-24 10:03:38 -0700

    modualize mysimplesrv.js

--- srv/mysimplesrv.js
+++ srv/mysimplesrv.js
@@ -1,24 +1,31 @@
 const cds = require("@sap/cds");
 
-const mysrvdemo = function(srv) {
-    srv.on("myfoobar", function(req,res) {
-        //return "Hello World!";
-        return req.data.msg;
-    })
+// async here is to make it synchronous, to avoid a mess of callbacks and promises
+async function _readStudentSrv(req) {
+    //const { StudentSrv } = srv.entities
+    const { StudentSrv } = this.entities
+    const ltabStudents = await cds.transaction(req).run(SELECT .from(StudentSrv))
+    if (ltabStudents.length > 0 ) {
+        ltabStudents[0].email = '2@gmail.com'
+        // $count has to be assigned or returned result will not dipslay. 'result.js' shows why.
+        // https://answers.sap.com/questions/13084938/how-can-i-implement-counttrue-support-with-an-exte.html
+        ltabStudents.$count = ltabStudents.length
+    }
+    return ltabStudents
+}
 
+
+function _myFoobar(req,res) {
+    //return "Hello World!";
+    return req.data.msg;
+}
+
+const mysrvdemo = function(srv) {
+    srv.on("myfoobar", _myFoobar)
+
+    //http://localhost:4004/mysrvdemoService/myfoobar(msg='Hi')
     //Below method(.on) is to override the default Service implementation
-     srv.on('READ', 'StudentSrv', async (req)=> {
-         //const { StudentSrv } = srv.entities
-         const { StudentSrv } = this.entities
-         const ltabStudents = await cds.transaction(req).run(SELECT .from(StudentSrv))
-         if (ltabStudents.length > 0 ) {
-             ltabStudents[0].email = '2@gmail.com'
-             // $count has to be assigned or returned result will not dipslay. 'result.js' shows why.
-             // https://answers.sap.com/questions/13084938/how-can-i-implement-counttrue-support-with-an-exte.html
-             ltabStudents.$count = ltabStudents.length
-         }
-         return ltabStudents
-     })
+     srv.on('READ', 'StudentSrv', _readStudentSrv)     
 }
 
 module.exports = mysrvdemo;
