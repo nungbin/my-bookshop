d6c67ec7e4f8e79696f1894dae968bbc916fbf45
shubinlin <shubinlin@gmail.com>
2021-11-26 16:18:01 -0700

    call the Catalog Service to retrieve a book and do the element binding for UI5 of project 2

--- app/project2/webapp/controller/Master.controller.js
+++ app/project2/webapp/controller/Master.controller.js
@@ -1,9 +1,11 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel",
 	"sap/m/MessageToast"
 ], function(
 	Controller,
-    MessageToast
+	JSONModel,
+	MessageToast
 ) {
 	"use strict";
 
@@ -12,31 +14,65 @@ sap.ui.define([
             this._defaultODataModel  = this.getOwnerComponent().getModel();
 			this._categoryODataModel = this.getOwnerComponent().getModel("categoryModel");
 			//Load JSON from a file
-			this._personJSONModel    = this.getOwnerComponent().getModel("personDataModel");
-			let _personJSON          = this._personJSONModel.getData();
-			_personJSON = Object.assign(_personJSON, { } );
+			this._bookJSONModel      = this.getOwnerComponent().getModel("personDataModel");
+			this.getView().setModel(this._bookJSONModel, "bookModel");
 
-			debugger;
-        },
+			// learned this from Vivek
+			// at this point, Odata call isn't made yet since it's async.
+			//this._categoryODataModel.metadataLoaded().then(this._onMetadataLoaded.bind(this));
+		},
+
+		// this is triggered by, to ensure metadata gets loaded
+		//		this.getOwnerComponent().getModel().metadataLoaded().then(this._onMetadataLoaded.bind(this));
+		//		if call is not blocked by SSO which can occur with WebIDE.
+		//		if call is blocked by SSO, _onMetadataLoaded will not be triggered at all. SSO needs to be turned off.
+		_onMetadataLoaded: function () {
+			// Fetching MetaModel from the OData model once metadata is loaded from 
+			//    this.getOwnerComponent().getModel().metadataLoaded().then(this._onMetadataLoaded.bind(this));
+			var oMetaData = this._categoryODataModel.getServiceMetadata(),
+				// iterate through array. common technique used in Javascript
+				oEntityType = oMetaData.dataServices.schema[0].entityType.filter(function (oItem) {
+					let tempItemName = '';
+					tempItemName = oItem.name;
+					if (tempItemName.toUpperCase() === "BOOKS") {
+						return oItem;
+					}
+				});
+				//_personJSON        = this._personJSONModel.getData();
+
+				//_personJSON = Object.assign(_personJSON, obj);	
+				//oEntityType[0].property.forEach(element => {  _personJSON[element.name] = ""; });
+
+				//tCategoryJSONModel = new sap.ui.model.json.JSONModel({
+				//	  "": oEntityType[0].property
+				//}),
+
+				//let _bookJSONModel = new JSONModel(_personJSON);
+				//this.getView().setModel(_bookJSONModel, "bookModel");
+		},	
 
         onClick: function (oEvent) {
-            //MessageToast.show("get Clicked");
-
             var sPath = "/Books";
             var that = this;
-            debugger;
 
 			this._categoryODataModel.read(sPath, {
 				success: function (oModelData, oResponse) {
-					/* do something */
-					//var oData = that.getView().getModel("trainingModel").getData();
 					//oData.BIC = oModelData.BIC;
 					//oData.IBAN_CLABE_OTHERS = oModelData.IBAN_CLABE_OTHERS;
 					//that.getView().getModel("trainingModel").setData(oData);  //refresh data on the screen
-                    debugger;
+					debugger;
+					if ( oModelData.results.length >= 1 ) {
+						let oData = that.getView().getModel("bookModel").getData();
+						//Below statement acts similar to ABAP's MOVE-CORRESPONDING
+						oData = Object.assign(oData, oModelData.results[0]);
+						that.getView().getModel("bookModel").setData(oData);
+						//Bind result to the simple form
+						var oProductDetailPanel = that.getView().byId("sfBooks");
+						oProductDetailPanel.bindElement({ path: "/", model: "bookModel" });
+					}
 				},
 				error: function (oError) {
-					/* do something */
+					MessageToast.show(this.getBundle().getText("MasterController.ERRORUnableToRetrieveABook"));
 					debugger;
 				}
 			});            
