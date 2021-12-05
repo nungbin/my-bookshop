c881626e4177061a42ed665063dc34d3cf27dbd3
shubinlin <shubinlin@gmail.com>
2021-12-05 11:31:29 -0700

    making case insensitive search work by changing schema directly in SQLITE

--- srv/server.js
+++ srv/server.js
@@ -1,6 +1,9 @@
 const cds = require("@sap/cds");
 const proxy = require("@sap/cds-odata-v2-adapter-proxy");
 
-cds.on("bootstrap", app => app.use(proxy()));
+cds.on("bootstrap", (app) => {
+    // https://www.npmjs.com/package/@sap/cds-odata-v2-adapter-proxy
+    app.use(proxy({caseInsensitive: true}));
+});
 
 module.exports = cds.server;
