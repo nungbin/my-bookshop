81e00c7b0585c6ce2e7a56bd48cf07b2d66fbf75
shubinlin <shubinlin@gmail.com>
2021-12-23 21:39:26 -0700

    add more game logic like initGame, pickRandomWord...etc

--- srv/wordlist.cds
+++ srv/wordlist.cds
@@ -3,6 +3,15 @@ using wordlist as wrd from '../db/wordlist';
 service wordlistsrv {
     entity wordList as projection on wrd.wordlist;
 
-    function getNoOfWords() returns Integer;
+    function initGame() returns Boolean;
+
+    function getNoOfWords() returns Boolean;
+
     function getWord(rowNum: Integer) returns String;
+
+    function pickRandomWord() returns Boolean;
+
+    function returnChosenWord() returns String;
+
+    function compareWords(enteredWord: String) returns Boolean;
 }
--- srv/wordlist.js
+++ srv/wordlist.js
@@ -1,20 +1,67 @@
 const cds = require("@sap/cds");
 
+
+function _getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+async function _getNoOfWords(req) {
+  const service = await cds.connect.to('db');
+  const txService = service.tx(req);
+  const sql = "select Count(1) from 'wordlist_wordlist'";
+
+  try {
+    const noOfWords = await txService.run(sql);
+    return noOfWords;
+  }
+  catch (error) {
+    //some errors
+    console.log(error);
+  }  
+}
+
+async function _getWord(req, rowNum) {
+  const service = await cds.connect.to('db');
+  const txService = service.tx(req);
+  const sql = "SELECT word FROM (" + 
+              "  SELECT " +
+              "    ROW_NUMBER() OVER ( " +
+              "      ORDER BY word " +
+              "    ) RowNum, " +
+              "    word " +
+              "  FROM " +
+              "    'wordlist_wordlist' " +
+              "  Order by word " +
+              ")" +
+              "where RowNum = " + rowNum;
+
+  try {
+    const word = await txService.run(sql);
+    return word;
+  }
+  catch (error) {
+    //some errors
+    console.log(error);
+  }
+}
+
+
 const wordListSrvImplementation = async function (srv) {
+    var pickedWord="",
+        counter=0;
+
+    srv.on("initGame", () => {
+      pickedWord = "";
+      return true;
+    });
+
+
     //http://localhost:4004/wordlistsrv/getNoOfWords()
     srv.on("getNoOfWords", async req => {
-        const service = await cds.connect.to('db');
-        const txService = service.tx(req);
-        const sql = "select Count(1) from 'wordlist_wordlist'";
-    
-        try {
-          const noOfWords = await txService.run(sql);
-          return noOfWords;
-        }
-        catch (error) {
-          //some errors
-          console.log(error);
-        }
+      //const noOfWords = await _getNoOfWords(req);
+      //return noOfWords;
+      //console.log(_getRandomInt(noOfWords[0]["Count(1)"]));
+      return true;
     });
 
     //http://localhost:4004/wordlistsrv/getWord(rowNum=3)
@@ -44,6 +91,23 @@ const wordListSrvImplementation = async function (srv) {
           console.log(error);
         }
     });
+
+    srv.on("pickRandomWord", async req => {
+      const noOfWords = await _getNoOfWords(req);
+      const word = await _getWord(req, _getRandomInt(noOfWords[0]["Count(1)"]));
+
+      pickedWord = word;
+      return true;
+    });
+
+    srv.on("returnChosenWord", () => {
+      return pickedWord;
+    });
+
+    srv.on("compareWords", (req) => {
+      const { enteredWord } = req.data;
+      return enteredWord;
+    })
 }
 
 module.exports = wordListSrvImplementation;
