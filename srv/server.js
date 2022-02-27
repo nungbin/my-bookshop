bf7c009c59213996a5cb389d829ac801936cca56
Bin Shu <shubinlin@gmail.com>
2022-02-26 19:56:18 -0700

    test CORS for project2/SMS

--- srv/server.js
+++ srv/server.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 
 cds.on("bootstrap", (app) => {
     // credit: https://answers.sap.com/questions/13204538/cap-angular-cors-block-on-cap-service.html
+    //   to allow CORS, it's setup on the server side, same as how it's setup in SICF in SAP.
     //console.debug("Use: cors middleware");
     console.log("Use: cors middleware");
     app.use(cors());    
